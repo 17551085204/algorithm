3e178d0eec04418b82a49dfd431b27560b9be549
hdk <2890241339@qq.com>
2020-11-24 23:05:07 +0800

    1
    1

---
+++ leetcode/src/com/leetcode/tempfilepath/leetcode/editor/cn/PowerOfTwo.java
@@ -0,0 +1,38 @@
+//给定一个整数，编写一个函数来判断它是否是 2 的幂次方。 
+//
+// 示例 1: 
+//
+// 输入: 1
+//输出: true
+//解释: 20 = 1 
+//
+// 示例 2: 
+//
+// 输入: 16
+//输出: true
+//解释: 24 = 16 
+//
+// 示例 3: 
+//
+// 输入: 218
+//输出: false 
+// Related Topics 位运算 数学 
+// 👍 262 👎 0
+
+package com.leetcode.tempfilepath.leetcode.editor.cn;
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        Solution solution = new PowerOfTwo().new Solution();
+        System.out.println("hello");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+
+
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
