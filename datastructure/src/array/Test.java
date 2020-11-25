dbf5a14f26c58ad3514f59d81bb0d87dda346e53
17551085204 <2890241339@qq.com>
2020-11-25 11:11:02 +0800

    1
    1

---
+++ datastructure/src/array/Test.java
@@ -0,0 +1,17 @@
+/*
+ * @Author hdk
+ * @QQ:2890241339
+ **/
+package array;
+
+public class Test {
+    public static void main(String[] args) {
+        int[]arr={2,3,4,2,1};
+        // 数组遍历
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+
+
+    }
+}
