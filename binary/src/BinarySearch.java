bbb971e74e456697acd4308b7ebb5ba718522b2d
17551085204 <2890241339@qq.com>
2020-11-28 16:14:19 +0800

    1
    1

--- binary/src/BinarySearch.java
+++ binary/src/BinarySearch.java
@@ -4,6 +4,7 @@
  **/
 
 public class BinarySearch {
+
     public static void main(String[] args) {
         int[]arr={0,1,2,3,4,5,6,7};
         int index = binarySearch(arr,14);
---
+++ mytest/src/my202011/my02.java
@@ -0,0 +1,25 @@
+/*
+ * @Author hdk
+ * @QQ:2890241339
+ **/
+package my202011;
+import org.junit.Test;
+
+/**
+ * 单元测试
+ */
+public class my02 {
+
+    @Test
+    public  void add(){
+        System.out.println("hello");
+        int a=1;
+        int b=2;
+        System.out.println(a+b);
+
+    }
+
+
+
+
+}
