1c8df154178a63f85d4de7092444c88067ce64de
17551085204 <2890241339@qq.com>
2020-12-25 11:40:26 +0800

    1
    1

--- mytest/src/my202012/my12.java
+++ mytest/src/my202012/my12.java
@@ -5,14 +5,62 @@
  **/
 package my202012;
 
+import java.util.Arrays;
+
 public class my12 {
     public static void main(String[] args) {
         int a=1;int b=2;
 //        swap(a,b);
 //        System.out.println("a="+a+",b="+b);
 
-        int[]res=swap2(a,b);
-        System.out.println("a="+res[0]+",b="+res[1]);
+//        int[]res=swap2(a,b);
+//        System.out.println("a="+res[0]+",b="+res[1]);
+
+//        System.out.println(jiecheng(3));
+
+//        double res=sum2n(63);
+//        System.out.println(res);
+
+        System.out.println(Arrays.toString(feibonaqie(14)));
+
+    }
+
+    public  static int[] feibonaqie(int n){
+        int[]res=new int[n];
+        res[0]=1;res[1]=1;
+        for (int i = 2; i < n; i++) {
+            res[i]=res[i-1]+res[i-2];
+        }
+
+        return res;
+    }
+
+
+
+
+
+    public static double sum2n(int n){
+        double res=0;
+        for (int i = 0; i <= n; i++) {
+            res+=mypower(2,i);
+        }
+        return res;
+    }
+
+    private static double mypower(int i, int n) {
+        double res=1;
+        for (int j = 0; j < n; j++) {
+            res*=i;
+        }
+        return res;
+    }
+
+
+    public static int jiecheng(int n){
+        if(n==1){
+            return 1;
+        }
+        return n*jiecheng(n-1);
 
     }
 
