1206480515f59782ab44a37902d93188434553a1
hdk <2890241339@qq.com>
2020-12-26 22:07:45 +0800

    1
    1

--- mytest/src/Hello.java
+++ mytest/src/Hello.java
@@ -6,10 +6,16 @@
 public class Hello {
     public static void main(String[] args) {
         // java最简单的输出语句
-        String str="hello world";
-        System.out.println(str);
-        System.out.println(1+1);
-        System.out.println("hello world 你好");
+//        String str="hello world";
+//        System.out.println(str);
+//        System.out.println(1+1);
+//        System.out.println("hello world 你好");
+
+        int i=1;
+        i=i++;
+        System.out.println(i);
+
+
 
     }
 }
