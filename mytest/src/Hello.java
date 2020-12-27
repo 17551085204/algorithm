f2c07ef6f5ef339809227e698354b508ddd1236d
hdk <2890241339@qq.com>
2020-12-27 22:45:36 +0800

    1
    1

--- mytest/src/Hello.java
+++ mytest/src/Hello.java
@@ -3,17 +3,27 @@
  * @QQ:2890241339
  **/
 
+import java.util.ArrayList;
+
 public class Hello {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // java最简单的输出语句
 //        String str="hello world";
 //        System.out.println(str);
 //        System.out.println(1+1);
 //        System.out.println("hello world 你好");
 
-        int i=1;
-        i=i++;
-        System.out.println(i);
+//        int i=1;
+//        i=i++;
+//        System.out.println(i);
+
+
+        ArrayList<int[]> ints = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            Thread.sleep(2000);
+            ints.add(new int[(int)(Math.random()*1000000)]);
+
+        }
 
 
 
