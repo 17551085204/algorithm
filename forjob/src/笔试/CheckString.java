dabd196cae4d5ba4c11c5d099df6b1f2eccacc4d
17551085204 <2890241339@qq.com>
2021-03-02 16:50:15 +0800

    1
    1

---
+++ forjob/src/笔试/CheckString.java
@@ -0,0 +1,15 @@
+/*
+ * @Author hdk
+ * @QQ:2890241339
+ * @Date 2021/3/2 0002
+ **/
+package 笔试;
+
+public class CheckString {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+    //
+
+}
