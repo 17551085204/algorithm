369ce63ff31bb13a90dd911cca93fb0f1165a304
17551085204 <2890241339@qq.com>
2021-03-02 16:58:08 +0800

    1
    1

--- forjob/src/笔试/CheckString.java
+++ forjob/src/笔试/CheckString.java
@@ -7,9 +7,35 @@ package 笔试;
 
 public class CheckString {
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+        checkString("helllo");
+
     }
 
-    //
+    // 编写一个字符串校验函数
+    /**
+     * helllo-->hello
+     *
+     * woooook->wook
+     */
+     public  static  String checkString(String inputStr){
+         char[] chars = inputStr.toCharArray();
+         StringBuilder stringBuilder = new StringBuilder();
+         for (int i = 0; i < chars.length-1;) {
+             stringBuilder.append(chars[i]);
+             if(chars[i]!=chars[i+1]){
+                 stringBuilder.append(chars[i+1]);
+             }
+
+
+
+
+         }
+
+
+
+         return "";
+     }
+
 
 }
